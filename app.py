071a8a0eee6592e0b1e8ff2aaaf5e3703001aad4
Igor <malyshevir02@gmail.com>
2025-05-04 16:21:34 +0700

    Добавлена поддержка credential

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 
 # Авторизация в Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+import os
+import json
+
+# Берём JSON из переменной окружения
+google_creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+google_creds_dict = json.loads(google_creds_json)
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(google_creds_dict, scope)
+
 client = gspread.authorize(creds)
 
 # Открываем таблицу по названию
@@ -20,4 +28,3 @@ def get_products():
 if __name__ == "__main__":
     app.run()
 
-fole = "credentials.json"
